6d9709b1ea91842f219c7ee4cb0ea2f10cd5e218
Warren <42559252+wsmarshall@users.noreply.github.com>
2022-07-21 12:52:05 -0700

    Finish first pass rock/paper/scissors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 function getComputerChoice() {
     let rawChoice = (Math.random() * 100);
     console.log("inside getComputerChoice, computer chooses: ", rawChoice);
@@ -15,10 +14,45 @@ function getComputerChoice() {
     }
 }
 
-function newRound(playerSelection = "rock", computerSelection = "rock") {
+function oneRound(playerSelection = "rock", computerSelection = "rock") {
+    playerSelection = playerSelection.toLowerCase(); //makes input case insensitive
+
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("You win! Paper beats Rock!");
+        return 1;
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("You win! Rock beats Scissors!");
+        return 1;
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("You win! Scissors beats Paper!");
+        return 1;
+    }
+    else if (playerSelection == computerSelection) {
+        console.log("A tie! " + playerSelection + " meets " + computerSelection + "!");
+        return 0;
+    }
+    else {
+        console.log("You lose! "+ computerSelection + " beats " + playerSelection + "!");
+        return 0;
+    }
 
 }
 
 function game() {
+    numWins = 0; //keeps track for best of 5
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Let's play rock, paper, scissors! Choose one of rock, paper, or scissors.");
+        computerSelection = getComputerChoice();
+        numWins += oneRound(playerSelection, computerSelection);
+    }
+
+    if (numWins > 2) {
+        console.log("You won best out of five! " + numWins + ":" + (5-numWins));
+    } else {
+        console.log("You lost best out of five! " + numWins + ":" + (5-numWins));
+    }
 
 }
